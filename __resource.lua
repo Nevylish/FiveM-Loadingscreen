95a88fe31d12cb32a3092d1cfcb41f223624db18
Akashite <twiiqfuzionz@gmail.com>
2020-04-19 23:30:07 +0200

    Rajout des fichiers

---
+++ __resource.lua
@@ -0,0 +1,21 @@
+files {
+    'index.html',
+    'music/load.ogg',
+    'img/logo.png',
+	'img/bg-1.jpg',
+    'img/bg-2.jpg',
+	'img/bg-3.jpg',
+    'img/timeout.png',
+	'css/bootstrap.css',
+    'css/owl.carousel.css',
+	'css/style.css',
+    'js/jquery.ajaxchimp.js',
+	'js/jquery.backstretch.min.js',
+    'js/jquery-1.11.0.min.js',
+	'js/lj-safety-first.js',
+    'js/owl.carousel.min.js',
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
