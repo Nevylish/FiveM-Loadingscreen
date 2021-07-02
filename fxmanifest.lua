8dd3a23274cbe56bea467f77ed0c227c8bdc96ca
Nevylish <nevylish@nevylish.fr>
2021-07-02 10:41:42 +0200

    add: logo in corner right

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,7 +10,8 @@ resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 files {
     'index.html',
 	'css/bootstrap.css',
-	'css/style.css'
+	'css/style.css',
+	'img/logo.png'
 }
 
 loadscreen 'index.html'
